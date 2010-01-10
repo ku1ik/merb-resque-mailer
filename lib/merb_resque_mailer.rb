db5242d404a414d67ec51f702e76f3a154779998
Marcin Kulik <marcin.kulik@gmail.com>
2010-01-10 14:41:52 +0100

    Fix aliasing of dispatch_and_deliver

--- lib/merb_resque_mailer.rb
+++ lib/merb_resque_mailer.rb
@@ -3,8 +3,30 @@ require 'resque'
 module Resque
   module Mailer
     
+    def self.excluded_environments=(*environments)
+      @@excluded_environments = environments && environments.flatten.collect! { |env| env.to_sym }
+    end
+    
+    def self.excluded_environments
+      @@excluded_environments ||= []
+    end
+
     def self.included(base)
       base.extend(ClassMethods)
+      base.class_eval do
+        alias_method :dispatch_and_deliver!, :dispatch_and_deliver
+        
+        def dispatch_and_deliver(method, mail_params)
+          puts "dispatching"
+          if ::Resque::Mailer.excluded_environments.include?(Merb.env.to_sym)
+            puts "old impl"
+            dispatch_and_deliver!(method, mail_params)
+          else
+            puts "putting to resque"
+            ::Resque.enqueue(self.class, @params, method, mail_params)
+          end
+        end
+      end
     end
  
     module ClassMethods
@@ -16,23 +38,6 @@ module Resque
         new(params).dispatch_and_deliver!(method, mail_params)
       end
     end
-      
-    def self.excluded_environments=(*environments)
-      @@excluded_environments = environments && environments.flatten.collect! { |env| env.to_sym }
-    end
-    
-    def self.excluded_environments
-      @@excluded_environments ||= []
-    end
 
-    alias :dispatch_and_deliver! :dispatch_and_deliver
-    
-    def dispatch_and_deliver(method, mail_params)
-      if ::Resque::Mailer.excluded_environments.include?(Merb.env.to_sym)
-        dispatch_and_deliver!(method, mail_params)
-      else
-        ::Resque.enqueue(self.class, @params, method, mail_params)
-      end
-    end
   end
 end
